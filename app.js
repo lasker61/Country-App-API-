ba098bf53e53982dd59b7be28bc27788d85143ae
lasker61 <laskernahid2@gmail.com>
2021-02-06 21:03:29 +0600

    completed

--- app.js
+++ app.js
@@ -7,13 +7,40 @@ const displayCountry = countries =>{
     for (let i = 0; i < countries.length; i++) {
         const country = countries [i];
         const countryDiv = document.createElement('div');
-        const h3= document.createElement('h3');
-        const p = document.createElement('p');
-        h3.innerText = country.name;
-        p.innerText = country.capital;
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(p);
+
+        /** this is one way */
+        // const h3= document.createElement('h3');
+        // const p = document.createElement('p');
+        // h3.innerText = country.name;
+        // p.innerText = country.capital;
+        // countryDiv.appendChild(h3);
+        // countryDiv.appendChild(p);
+
+        /**this is another way of doing */
+        countryDiv.className = 'country';
+        const countryInfo =`<h3 class ="country-name">${country.name}</h3> 
+                            <p>${country.capital}</p>
+                            <button class ="btn" onClick="displayCountryDetail('${country.name}')">Details</button>
+         
+        `
+        countryDiv.innerHTML =countryInfo;
         countriesDiv.appendChild(countryDiv);
         
     }
+}
+const displayCountryDetail = name =>{
+    const url =`https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => renderCountryInfo(data[0]));
+}
+const renderCountryInfo = country =>{
+    console.log(country);
+    const countryDiv = document.getElementById('country-details');
+    countryDiv.innerHTML =` <h3>${country.name}</h3>
+                            <p>Population: ${country.population}</p> 
+                            <p>Area: ${country.area}</p> 
+                            <img src="${country.flag}">
+                           
+    `
 }
