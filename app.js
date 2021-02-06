95eef5f9d9e852874a3bed0990da12b6ac52107e
lasker61 <laskernahid2@gmail.com>
2021-02-06 19:46:43 +0600

    updated

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ fetch('https://restcountries.eu/rest/v2/all')
 .then(data => displayCountry(data));
 
 const displayCountry = countries =>{
-    console.log(countries);
+    const countriesDiv =document.getElementById('countries');
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries [i];
+        const countryDiv = document.createElement('div');
+        const h3= document.createElement('h3');
+        const p = document.createElement('p');
+        h3.innerText = country.name;
+        p.innerText = country.capital;
+        countryDiv.appendChild(h3);
+        countryDiv.appendChild(p);
+        countriesDiv.appendChild(countryDiv);
+        
+    }
 }
